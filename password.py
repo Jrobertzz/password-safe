4ad64a52f9f2f509dc3f3216787f0bf64764561a
Jrobertzz <robertsjw2@vcu.edu>
2017-04-11 16:11:30 -0400

    added password method bound to Enter event

--- password.py
+++ password.py
@@ -34,18 +34,24 @@ class gui(Tk):
     	self.clear()
     	i = 0
     	user = Label(text = username)
-        self.widgets.append(user)
+    	self.widgets.append(user)
     	user.configure(bg='gray24')
     	user.place(width = 50, height = 10,relx=.5, rely=0.48, anchor=CENTER)
 
-    	password = Entry();
-        self.widgets.append(password)
-    	password.place(width = 100, height = 20,relx=.5, rely=0.52, anchor=CENTER)
+    	self.password = Entry();
+    	self.widgets.append(self.password)
+    	self.password.place(width = 100, height = 20,relx=.5, rely=0.52, anchor=CENTER)
+    	self.password.bind('<Return>', self.OnEnter)
 
     def clear(self):
     	for widget in self.widgets:
     		widget.destroy()
+    	self.widgets = []
 
+    def OnEnter(self, event):
+    	__password = self.password.get()
+    	self.password.delete(0, END)
+    	print(__password)
 
 
 
